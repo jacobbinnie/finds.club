6d27e563cb99cc362b00b6d81d20f301a289d676
Jacob <hello@jacobbinnie.com>
2023-08-05 19:08:48 +1200

    feat: add timers to api calls to save bandwidth

--- src/app/hooks/useAddresses.ts
+++ src/app/hooks/useAddresses.ts
@@ -5,31 +5,49 @@ const useAddresses = (searchQuery: string | undefined) => {
   const [error, setError] = useState<string | null>(null);
   const [loading, setLoading] = useState<boolean>(false);
   const [data, setData] = useState<AddressableAddress[]>([]);
+  const [timerId, setTimerId] = useState<NodeJS.Timeout | null>(null);
 
   useEffect(() => {
     setLoading(true);
     setError(null);
 
     if (searchQuery && searchQuery.length > 2) {
-      fetch(`/api/suggest-address?q=${searchQuery}`)
-        .then((response) => response.json())
-        .then((data) => {
-          if (isAddressableAddressArray(data)) {
-            setData(data);
-          } else {
-            setError("Received data is not in the expected format.");
-          }
-        })
-        .catch(() => {
-          setError("An error occurred while fetching data.");
-        })
-        .finally(() => {
-          setLoading(false);
-        });
+      // Clear the existing timer, if any
+      if (timerId) {
+        clearTimeout(timerId);
+      }
+
+      // Set a new timer
+      const newTimer = setTimeout(() => {
+        fetch(`/api/suggest-address?q=${searchQuery}`)
+          .then((response) => response.json())
+          .then((data) => {
+            if (isAddressableAddressArray(data)) {
+              setData(data);
+            } else {
+              setError("Received data is not in the expected format.");
+            }
+          })
+          .catch(() => {
+            setError("An error occurred while fetching data.");
+          })
+          .finally(() => {
+            setLoading(false);
+          });
+      }, 1000); // 1 second delay
+
+      setTimerId(newTimer);
     } else {
       setLoading(false);
       setData([]);
     }
+
+    // Clear the timer when the component unmounts or when searchQuery changes
+    return () => {
+      if (timerId) {
+        clearTimeout(timerId);
+      }
+    };
   }, [searchQuery]);
 
   return { error, loading, data, setData };
